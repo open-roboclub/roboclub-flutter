09f4af82c3dd42e7af6f9f834f237f03c0e9cf8a
samiksha99 <agrawalsamiksha25@gmail.com>
2020-10-20 02:30:42 +0530

    contributors screen

--- lib/widgets/contribution_card.dart
+++ lib/widgets/contribution_card.dart
@@ -6,10 +6,12 @@ class ContriCard extends StatelessWidget {
   Widget build(BuildContext context) {
     var vpH = getViewportHeight(context);
     var vpW = getViewportWidth(context);
+    TextStyle _titlestyle =
+        TextStyle(fontWeight: FontWeight.bold, fontSize: vpH * 0.025);
     return  Padding(
           padding: EdgeInsets.all(25.0),
           child: Container(
-            height: vpH * 0.15,
+            height: vpH * 0.13,
             width: vpW * 0.85,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
@@ -17,14 +19,64 @@ class ContriCard extends StatelessWidget {
               boxShadow: [
                 BoxShadow(
                   color: Colors.blueGrey[200],
-                  // offset: Offset(2, 2),
                   blurRadius: 5.0,
-                  // spreadRadius: 1.0,
                   offset: Offset(0.0, 0.75)
                 ),
               ],
-        ),
-          ),
-        );
+             ),
+             child:Column(children: [
+             Padding(padding: EdgeInsets.all(15.0),
+             child:Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  flex: 1,
+                  child:CircleAvatar(
+                  radius: 21,
+                  backgroundColor: Colors.black,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage('assets/img/placeholder.jpg'),
+                  ),
+                )
+                ),
+                Flexible(
+                  flex: 2,
+                  fit: FlexFit.tight,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal:20.0,vertical: 5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "La'chef",
+                          overflow: TextOverflow.ellipsis,
+                          style: _titlestyle,
+                        ),
+                        Text(
+                          "-Contibuted to",
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child:Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "Contribution",
+                   overflow: TextOverflow.ellipsis,
+                  ),
+                )
+            ),
+            ])
+             )
+             );
   }
 }
