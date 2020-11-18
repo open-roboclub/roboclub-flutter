8870d7cc7693abe322d45cbfa105c661fcc11aa7
Rishabh <princerishabh.sharma400@gmail.com>
2020-11-18 19:57:30 +0530

    bash script rewrite

--- android/app/decrypt_secret.sh
+++ android/app/decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
-# mkdir $HOME/secrets
+mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICE_SECRET" \
---output /google-services.json google-services.json.gpg
+--output $HOME/secrets/google-services.json google-services.json.gpg
