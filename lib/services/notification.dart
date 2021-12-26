c966830a678a78440a81fdb5d11f0dcbf7206bbe
samiksha99 <agrawalsamiksha25@gmail.com>
2021-12-26 14:46:05 +0530

    registration form

--- lib/services/notification.dart
+++ lib/services/notification.dart
@@ -43,13 +43,15 @@ class NotificationService {
     List<String> tokens = await getFCMTokens();
     final Map<String, dynamic> body = {
       // "to": 'fwGkbdNJRiSKRkOSQBMYi6:APA91bFG9P6YsOBaLJD8kEpUBd1VHn1W7SSrSehVdQ10X4hfQDxf32K2gUchpc1SYIwP78fxVOEecihsKXfxkdMDFtauaYLaFL_MHMhh4pmW36YYql_dskatPRhKuPFB6FCeD5IgQLwh',
-      "registration_ids": ['fwGkbdNJRiSKRkOSQBMYi6:APA91bFG9P6YsOBaLJD8kEpUBd1VHn1W7SSrSehVdQ10X4hfQDxf32K2gUchpc1SYIwP78fxVOEecihsKXfxkdMDFtauaYLaFL_MHMhh4pmW36YYql_dskatPRhKuPFB6FCeD5IgQLwh', 'c9PwIj7jT6y7GxCyx7_6o7:APA91bGnW3uxeoaoqeFM7vxrWH2DtjXHve3f6QfKcERo7dJ5U4RMr3UNbZXZ96wbN11vpliiIQf6McSWKA-7ZiSg1utj3eE1Rf0lM8UrvR4AH9ULDEY_xySMG5bEwkaS1EzopThv8Ktm', 'dCydb-A8RDGwthLK2VR5JI:APA91bH0nF2NwSoGc8B849jIJZQmYhbuGWSrCyKfW81pvwuQIdmuKA2jKAB37eZSYp76uvk1VU2fUE2aiyKwhCRRUK0GEIXhLEpUeSgeO_nwQg5DDFt8ntuHJiijfSc0NUBQ34z2n5J3', 'e4FZT9cNSj6Z8WYAqZV3oP:APA91bHUlyYJ4I9gsXdA3yS1_eAKK_GylODWgUKWvXtRZ5XnYCAbaPoG6NmgGsMnyc9ylkvr-mY3E7VgBrXTC3QKIi0VObeIUBgYBTPi_C21lscS0CdkeTg7G_3njLqNcaq4HoQwbGjL', 'd4jwa-FJTFqiZdEa8S9996:APA91bEiezr7Kkh1XuSNWMNblaapBPDRe8M3gwbKN1vNIhb0kuHqy3kzggGmuygFF1h5ntYG4H1IkUGwSC5Yqwxs5F2wcRz95xnc_QtBETXwpFWGtMRRihgKHBREu-N-IK22et6IS010'],
+      "registration_ids": tokens,
+      // ['fwGkbdNJRiSKRkOSQBMYi6:APA91bFG9P6YsOBaLJD8kEpUBd1VHn1W7SSrSehVdQ10X4hfQDxf32K2gUchpc1SYIwP78fxVOEecihsKXfxkdMDFtauaYLaFL_MHMhh4pmW36YYql_dskatPRhKuPFB6FCeD5IgQLwh', 'c9PwIj7jT6y7GxCyx7_6o7:APA91bGnW3uxeoaoqeFM7vxrWH2DtjXHve3f6QfKcERo7dJ5U4RMr3UNbZXZ96wbN11vpliiIQf6McSWKA-7ZiSg1utj3eE1Rf0lM8UrvR4AH9ULDEY_xySMG5bEwkaS1EzopThv8Ktm', 'dCydb-A8RDGwthLK2VR5JI:APA91bH0nF2NwSoGc8B849jIJZQmYhbuGWSrCyKfW81pvwuQIdmuKA2jKAB37eZSYp76uvk1VU2fUE2aiyKwhCRRUK0GEIXhLEpUeSgeO_nwQg5DDFt8ntuHJiijfSc0NUBQ34z2n5J3', 'e4FZT9cNSj6Z8WYAqZV3oP:APA91bHUlyYJ4I9gsXdA3yS1_eAKK_GylODWgUKWvXtRZ5XnYCAbaPoG6NmgGsMnyc9ylkvr-mY3E7VgBrXTC3QKIi0VObeIUBgYBTPi_C21lscS0CdkeTg7G_3njLqNcaq4HoQwbGjL', 'd4jwa-FJTFqiZdEa8S9996:APA91bEiezr7Kkh1XuSNWMNblaapBPDRe8M3gwbKN1vNIhb0kuHqy3kzggGmuygFF1h5ntYG4H1IkUGwSC5Yqwxs5F2wcRz95xnc_QtBETXwpFWGtMRRihgKHBREu-N-IK22et6IS010'],
       "data": {"screen": screen},
+      "dry_run": true,
       "notification": {
         "title": title,
         "body": msg,
         "click_action": "FLUTTER_NOTIFICATION_CLICK",
-        "image": img
+        "image": img,
       }
     };
 
